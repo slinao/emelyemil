983b439eba969d4ac5cd654fea25415d8b55413c
slinao <linaoegren@gmail.com>
2025-09-24 20:42:20 +0200

    Update form-process.php

--- php/form-process.php
+++ php/form-process.php
@@ -4,21 +4,21 @@ $errorMSG = "";
 
 // NAME
 if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
+    $errorMSG = "Du måste skriva in ditt namn :) ";
 } else {
     $name = $_POST["name"];
 }
 
 // EMAIL
 if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
+    $errorMSG .= "Ange din mailadress, tack! ";
 } else {
     $email = $_POST["email"];
 }
 
 // MSG Guest
 if (empty($_POST["guest"])) {
-    $errorMSG .= "Subject is required ";
+    $errorMSG .= "Välj från listan :) ";
 } else {
     $guest = $_POST["guest"];
 }
@@ -26,7 +26,7 @@ if (empty($_POST["guest"])) {
 
 // MSG Event
 if (empty($_POST["event"])) {
-    $errorMSG .= "Subject is required ";
+    $errorMSG .= "Välj från listan. ";
 } else {
     $event = $_POST["event"];
 }
@@ -34,42 +34,42 @@ if (empty($_POST["event"])) {
 
 // MESSAGE
 if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
+    $errorMSG .= "Skriv ditt meddelande här! ";
 } else {
     $message = $_POST["message"];
 }
 
-
-$EmailTo = "armanmia7@gmail.com";
-$Subject = "New Message Received";
+//TODO: Mail till?
+//$EmailTo = "armanmia7@gmail.com";
+$Subject = "OSA BRÖLLOP 16/5-26";
 
 // prepare email body text
 $Body = "";
-$Body .= "Name: ";
+$Body .= "Namn: ";
 $Body .= $name;
 $Body .= "\n";
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "guest: ";
+$Body .= "gäst: ";
 $Body .= $guest;
 $Body .= "\n";
 $Body .= "event: ";
 $Body .= $event;
 $Body .= "\n";
-$Body .= "Message: ";
+$Body .= "Meddelande: ";
 $Body .= $message;
 $Body .= "\n";
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($EmailTo, $Subject, $Body, "Från:".$email);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
-   echo "success";
+   echo "Meddelande skickat!";
 }else{
     if($errorMSG == ""){
-        echo "Something went wrong :(";
+        echo "Något gick fel :(";
     } else {
         echo $errorMSG;
     }
